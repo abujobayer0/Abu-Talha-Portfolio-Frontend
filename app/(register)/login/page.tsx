48fc485d5d3185527697d3adef6f323161812c41
abu jobayer <zubayer.munna.dev@gmail.com>
2025-10-29 13:15:45 +0600

    implement loading state for login page using Suspense and Loader component

--- app/(register)/login/page.tsx
+++ app/(register)/login/page.tsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
+import Loader from '@/app/(home)/_components/ui/RLoader/loader';
 import LoginForm from '@/components/module/Login';
 
 export default function LoginPage() {
   return (
-    <div className='h-screen m-auto w-full'>
-      <LoginForm />
-    </div>
+    <Suspense fallback={<Loader />}>
+      <div className='h-screen m-auto w-full'>
+        <LoginForm />
+      </div>
+    </Suspense>
   );
 }
